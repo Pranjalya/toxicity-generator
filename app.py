421f95cebff9b4cbc26f81d5f37e57ada510446a
Pranjalya <pranjalyawarrior@gmail.com>
2020-08-03 00:31:26 +0530

    Flask set

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, jsonify
+import gdown
+from fastai.basic_train import load_learner
+from flask_cors import CORS,cross_origin
+import random
+
+app = Flask(__name__)
+CORS(app, support_credentials=True)
+
+# load the learner
+url = 'https://drive.google.com/uc?id=13a-VopEnvc2YTji4SbmhqxFGkxnVjw3j'
+output = './model/usemodel/export.pkl'
+gdown.download(url, output, quiet=False)
+learn = load_learner('./model/usemodel')
+
+
+def get_sentences(request):
+    """
+    Function to return new set of abuses.
+    """
+    shower_me = 35
+    toxic_counter = 0
+    all_toxics = []
+
+    for i in range(250):
+        sent = learn.predict("xxbos ", n_words=50, temperature=0.8)
+        sents = sent.split("xxbos ")
+        sents = sents[1:-1]
+
+        for sent in sents:
+            sent = sent.replace("xxbos","").strip()
+            if(sent):
+                all_toxics.append(sent)
+                toxic_counter = toxic_counter+1
+
+        if toxic_counter > shower_me:
+            break
+    
+    toxics = random.sample(all_toxics, 10)
+
+    return all_toxics
+
+
+# route for prediction
+@app.route('/gettoxic', methods=['POST'])
+def predict():
+    return jsonify(predict_single(request))
+
+if __name__ == '__main__':
+    app.run()
