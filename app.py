77c7a01d75363d337ca233434b2e4c6cba1399bb
Saurabh Bhagvatula <saurabhbh21@.com>
2020-08-03 11:42:26 +0530

    GPU enabled

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS,cross_origin
 
 
-fastai.device = torch.device('cpu')
+#fastai.device = torch.device('cpu')
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 import requests
 
 if __name__ == "__main__":
-    url = 'http://127.0.0.1:5000/gettoxic'
+    url = 'https://toxicity-generator.onrender.com/gettoxic'
 
     response =  requests.post(url)
+    import pdb; pdb.set_trace()
     result = response.json()
     pritn(result)
