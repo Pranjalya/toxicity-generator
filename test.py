0f72382e66f9650e5714d77a7900f61a5ab714af
Saurabh Bhagvatula <saurabhbh21@.com>
2020-08-03 12:23:05 +0530

    port updated test

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 
 if __name__ == "__main__":
-    url = 'http://15.207.99.158:5000/gettoxic'
+    url = 'http://15.207.99.158:6000/gettoxic'
 
     response =  requests.post(url)
     import pdb; pdb.set_trace()
